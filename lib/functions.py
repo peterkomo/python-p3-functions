e43a4b626cb99f8ee8baf0c9df57ea01c8318bc9
peterkomo <mwangipeterkomo@gmail.com>
2023-08-14 22:27:46 +0300

    tried

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
-def greet(name):
-    pass
+def greet(name="Naureen"):
+    print(f"Hello,${name}!")
+    greet()
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"hello,${name}!")
+    greet_with_default()
 
-def add(num1, num2):
-    pass
+def add(num1=1, num2=2):
+    print(num1+num2)
+    add()
 
 def halve(number):
     pass
